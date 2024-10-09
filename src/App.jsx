4956f5550dba8cc3c2cbc315d911235bc1ccc50a
pate <141310795+TaiNgonheng@users.noreply.github.com>
2024-10-09 02:44:46 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+export default function App() {
+  const [advice, setadvice] = useState("");
+  const [count, setcount] = useState(0);
+
+  async function getadvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setadvice(data.slip.advice);
+    setcount((c) => c + 1);
+  }
+  useEffect(function () {
+    getadvice();
+  }, []);
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getadvice}>Get Advice</button>
+    </div>
+  );
+}
+
+function Message() {
+  return (
+    <p>
+      you have read <strong>{count}</strong>pieces of advice.
+    </p>
+  );
+}
